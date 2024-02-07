81763978345f56a2f0a427285a5916f08de7be5e
ANURAG RAI <84726281+anurag87204@users.noreply.github.com>
2024-02-07 11:10:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ ScrollReveal().reveal('.home-content p, .about-content, .skills-right', { origin
 
 /////////////typed js////////////////
 const typed = new Typed('.multiple-text',{
-    strings: ['Web Developer','Web App' , 'Data Analytics'],
+    strings: ['FULL STACK Developer','Web App' , 'Data Analytics'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
